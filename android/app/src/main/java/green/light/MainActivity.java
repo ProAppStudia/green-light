bb02528d23eace334f9ecaa441646c1472f0d70f
ProAppStudia <proapp.studia@gmail.com>
2025-08-23 01:56:33 +0300

    clear Ionic + Angular

---
+++ android/app/src/main/java/green/light/MainActivity.java
@@ -0,0 +1,5 @@
+package green.light;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'green.light',
+  appName: 'green-light',
+  webDir: 'www'
+};
+
+export default config;
