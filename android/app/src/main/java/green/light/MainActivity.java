d62e885c5f95c49ff389208b12f2f2f06ff3fbc3
ProAppStudia <proapp.studia@gmail.com>
2026-04-14 22:42:01 +0300

    final map

--- android/app/src/main/java/green/light/MainActivity.java
+++ android/app/src/main/java/green/light/MainActivity.java
@@ -1,7 +1,7 @@
 package green.light;
 
+import android.content.Context;
 import android.content.res.Configuration;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import com.getcapacitor.BridgeActivity;
@@ -9,24 +9,21 @@ import com.getcapacitor.BridgeActivity;
 public class MainActivity extends BridgeActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // Prevent system font scale from affecting WebView text
-        if (bridge != null && bridge.getWebView() != null) {
-            bridge.getWebView().getSettings().setTextZoom(100);
-        }
-    }
-
-    @Override
-    public Resources getResources() {
-        Resources res = super.getResources();
-        Configuration config = new Configuration(res.getConfiguration());
-        // Lock font scale to default
+    protected void attachBaseContext(Context newBase) {
+        Configuration config = new Configuration(newBase.getResources().getConfiguration());
         config.fontScale = 1.0f;
-        // Lock display density to default (prevents "Display size" setting from enlarging layout)
         if (config.densityDpi > DisplayMetrics.DENSITY_DEVICE_STABLE) {
             config.densityDpi = DisplayMetrics.DENSITY_DEVICE_STABLE;
         }
-        return createConfigurationContext(config).getResources();
+        Context context = newBase.createConfigurationContext(config);
+        super.attachBaseContext(context);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (bridge != null && bridge.getWebView() != null) {
+            bridge.getWebView().getSettings().setTextZoom(100);
+        }
     }
 }
